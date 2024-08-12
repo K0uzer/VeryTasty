4ed81525f5b6f4afc89b66a0293004b95bd25dba
Alex <87208475+K0uzer@users.noreply.github.com>
2024-08-12 16:54:14 +0300

    feat: Add Frame in layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import styles from "./page.module.css";
+'use client'
+import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <main className={styles.main}></main>
-  );
+    return <main className={styles.main}>Data</main>
 }
